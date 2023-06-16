df7cc6bb97801c62521036a32c91870df2dead02
ade guntur <ade.guntur@bnisekuritas.co.id>
2023-06-16 09:45:29 +0700

    stable version

---
+++ application/views/templates/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 0.1.0
+    </div>
+
+</footer>
+
+<div class="control-sidebar-bg"></div>
+</div>
+
+</body>
+
+</html>
---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,12 @@
+<header class="main-header">
+    <a href="<?php echo base_url('') ?>" class="logo">
+        <span class="logo-mini"><b>STF</b></span>
+        <span class="logo-lg"><b>StockFlow</b></span>
+    </a>
+    <nav class="navbar navbar-static-top">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+    </nav>
+</header>
